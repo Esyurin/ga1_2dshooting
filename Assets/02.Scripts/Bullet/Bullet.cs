f2ae528c5265c7404b38a25c52ee3a5febe75b91
Esyurin <dbtmdgns2031@gmail.com>
2026-09-02 14:05:57 +0900

    feat: 탄환 이동 구현

---
+++ Assets/02.Scripts/Bullet/Bullet.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private float _bulletSpeed = 5f;
+    [SerializeField] private float _bulletYPositionMax = 6f;
+    
+    private void Update()
+    {
+        Move();        
+
+        if (transform.position.y < _bulletYPositionMax)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Move()
+    {
+        transform.Translate(_bulletSpeed * Time.deltaTime * Vector3.up);
+    }
+}
