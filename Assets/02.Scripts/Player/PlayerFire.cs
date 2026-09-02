b537292da32b9d5c84831b0e0d881a0bab9cac61
Esyurin <dbtmdgns2031@gmail.com>
2026-09-02 15:03:56 +0900

    feat: 탄환 쿨타임 구현

--- Assets/02.Scripts/Player/PlayerFire.cs
+++ Assets/02.Scripts/Player/PlayerFire.cs
@@ -5,11 +5,16 @@ public class PlayerFire : MonoBehaviour
     public GameObject BulletPrefab;
     public Transform LeftFirePoint;
     public Transform RightFirePoint;
+
+    private float _timer = 0f;
+    public float Cooltime = 0.3f;
     
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        _timer += Time.deltaTime;
+        if (_timer >= Cooltime && Input.GetKeyDown(KeyCode.Space))
         {
+            _timer = 0f;
             Fire();
         }
     }
