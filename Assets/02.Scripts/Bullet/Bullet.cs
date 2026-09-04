5c6edacec5e56644799a9d9ffe397164cc41a751
Esyurin <dbtmdgns2031@gmail.com>
2026-09-04 16:08:14 +0900

    feat: 데드존 기반 화면 밖 객체 제거 구현

--- Assets/02.Scripts/Bullet/Bullet.cs
+++ Assets/02.Scripts/Bullet/Bullet.cs
@@ -4,16 +4,10 @@ public class Bullet : MonoBehaviour
 {
     [SerializeField] private float _bulletAttackPower = 1f;
     [SerializeField] private float _bulletSpeed = 5f;
-    [SerializeField] private float _bulletYPositionMax = 6f;
 
     private void Update()
     {
         Move();
-
-        if (transform.position.y > _bulletYPositionMax)
-        {
-            Destroy(gameObject);
-        }
     }
 
     private void Move()
