b1c45dc8b2144020990882849e4a7f81893e5cb5
Esyurin <dbtmdgns2031@gmail.com>
2026-09-08 15:34:15 +0900

    feat: 플레이어 폭탄 사용 기초 로직 추가
    - B키 입력 시 폭탄을 생성하는 PlayerBomb 스크립트 추가
    - 폭탄의 위쪽 이동 로직 구현
    - 플레이어 이동 궤적의 위치와 표시 설정 조정

---
+++ Assets/02.Scripts/Bomb/Bomb.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        transform.Translate(Vector3.up * Time.deltaTime);
+    }
+}
---
+++ Assets/02.Scripts/Player/PlayerBomb.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerBomb : MonoBehaviour
+{
+    [SerializeField] private GameObject _bombPrefab;
+
+    [SerializeField] private float _coolTime = 10f;
+
+    private float _timer;
+    private bool _bombReady = true;
+
+    private void Awake()
+    {
+        Debug.Assert(_bombPrefab != null, "(PlayerBomb) Bomb Prefab is null");
+    }
+
+    private void Update()
+    {
+        CheckCoolTime();
+
+        if (_bombReady && Input.GetKeyDown(KeyCode.B))
+        {
+            DropBomb();
+        }
+    }
+
+    private void CheckCoolTime()
+    {
+        if (!_bombReady)
+        {
+            _timer += Time.deltaTime;
+        }
+        else
+        {
+            _timer = 0f;
+        }
+    }
+
+    private void DropBomb()
+    {
+        _bombReady = false;
+        Instantiate(_bombPrefab, transform.position, Quaternion.identity);
+    }
+}
