cf7215b78265d70275cf6e02182cc59efca48684
Esyurin <dbtmdgns2031@gmail.com>
2026-09-14 12:12:16 +0900

    refactor(ui): 버튼 클릭 효과를 공통 컴포넌트로 분리
    - UI_ButtonClick에서 클릭 시 확대 효과와 사운드 재생 처리
    - UI_AutoButton의 주기적 펄스 효과와 사운드 재생 로직 제거

--- Assets/02.Scripts/UI/UI_AutoButton.cs
+++ Assets/02.Scripts/UI/UI_AutoButton.cs
@@ -16,10 +16,6 @@ public class UI_AutoButton : MonoBehaviour
     private PlayerFire _playerFire;
     private PlayerAutoMove _playerAutoMove;
 
-    private Vector3 _defaultScale;
-    private float _pulseScale = 0.1f;
-    private float _pulseSpeed = 2f;
-
     private void Awake()
     {
         _image = GetComponent<Image>();
@@ -28,23 +24,11 @@ public class UI_AutoButton : MonoBehaviour
         _player = FindAnyObjectByType<Player>();
         _playerFire = _player.GetComponent<PlayerFire>();
         _playerAutoMove = _player.GetComponent<PlayerAutoMove>();
-
-        _defaultScale = transform.localScale;
     }
 
     private void Start()
     {
         _isAuto = _playerFire.IsAuto;
-    }
-
-    private void Update()
-    {
-        PlayButtonPulse();
-    }
-
-    private void PlayButtonPulse()
-    {
-        transform.localScale = _defaultScale + _pulseScale * Mathf.Sin(_pulseSpeed * Time.time) * Vector3.one;
     }
 
     public void AutoToggle()
@@ -53,6 +37,5 @@ public class UI_AutoButton : MonoBehaviour
         _playerFire.ToggleAuto();
         _playerAutoMove.enabled = _isAuto;
         _image.sprite = _isAuto ? _onSprite : _offSprite;
-        _audioSource.Play();
     }
 }
---
+++ Assets/02.Scripts/UI/UI_ButtonClick.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_ButtonClick : MonoBehaviour
+{
+    private AudioSource _audioSource;
+    private Button _button;
+
+    private Vector3 _defaultScale;
+    private bool _isBump;
+    private float _bumpScale = 1.1f;
+    [SerializeField] private float _bumpSpeed = 2f;
+    private float _bumpTime;
+
+    private void Awake()
+    {
+        _defaultScale = transform.localScale;
+        _audioSource = GetComponent<AudioSource>();
+        _button = GetComponent<Button>();
+        _button.onClick.AddListener(PlayButtonBump);
+        _button.onClick.AddListener(PlayButtonSound);
+    }
+
+    private void Update()
+    {
+        if (_isBump)
+        {
+            _bumpTime += Mathf.Sin(Mathf.PI * Time.deltaTime * _bumpSpeed);
+            transform.localScale = Vector3.Lerp(_defaultScale, _bumpScale * _defaultScale, _bumpTime);
+        }
+
+        if (_bumpTime > Mathf.PI)
+        {
+            _isBump = false;
+            _bumpTime = 0;
+        }
+    }
+
+    private void PlayButtonBump()
+    {
+        _isBump = true;
+        transform.localScale = _defaultScale;
+    }
+
+    private void PlayButtonSound()
+    {
+        _audioSource.Play();
+    }
+}
